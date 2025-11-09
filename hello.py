2ede03b96dd3228c7e0ce32da6759b8498f6e7cd
Khushi-Kumari947 <kk12gcs@gmail.com>
2025-11-09 23:33:18 +0530

    Initial commit

--- hello.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyBwb8k_PCKo94gwwL9dE12ZtHfVKJQCpt8")
-
-response = genai.embed_content(model="models/embedding-001", content="Machine learning and data analysis")
-print(len(response["embedding"]))

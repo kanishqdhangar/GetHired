1dec60f8d6f1c29fc83cff09d95b2701d7883462
Kanishq Dhangar <dhangarkanishq@gmail.com>
2025-11-16 23:46:07 +0530

    Resolved Error

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware 
+import sys
+import os
 # Import files based on your confirmed structure
-from .ml_pipeline.db_connector import engine
-from .ml_pipeline.models import Base, init_db 
-from .routers.recommend_router import router as recommend_router
-from .routers.auth_router import auth_router
-from .routers.recruiter_router import recruiter_router
-from .routers.student_router import student_router 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from ml_pipeline.db_connector import engine
+from ml_pipeline.models import Base, init_db 
+from routers.recommend_router import router as recommend_router
+from routers.auth_router import auth_router
+from routers.recruiter_router import recruiter_router
+from routers.student_router import student_router 
 
 # --- 1. Lifespan Events (Database Initialization) ---
 
